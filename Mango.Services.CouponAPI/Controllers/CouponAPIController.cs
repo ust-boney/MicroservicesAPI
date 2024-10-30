0552b25bff18dfe6d498d3a6b481b3f6a86fe097
boney84 <boney0084@gmail.com>
2024-10-30 21:21:52 +0530

    Added microservice coupon webapi

---
+++ Mango.Services.CouponAPI/Controllers/CouponAPIController.cs
@@ -0,0 +1,158 @@
+﻿using AutoMapper;
+using Mango.Services.CouponAPI.Data;
+using Mango.Services.CouponAPI.Models;
+using Mango.Services.CouponAPI.Models.Dto;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mango.Services.CouponAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CouponAPIController : ControllerBase
+    {
+        private readonly AppDbContext _dbContext;
+        readonly ResponseDto _responseDto;
+        readonly IMapper _mapper;
+
+        public CouponAPIController(AppDbContext dbContext,IMapper mapper)
+        {
+            _dbContext = dbContext;
+            _responseDto = new ResponseDto();
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public ResponseDto GetAllCoupons()
+        {
+            try
+            {
+                IEnumerable<Coupon> coupons = _dbContext.Coupons.ToList();
+                IEnumerable<CouponDto> couponDtoList= _mapper.Map<IEnumerable<CouponDto>>(coupons);
+                _responseDto.Result = couponDtoList;
+                _responseDto.IsSuccess = true;
+            }
+            catch (Exception ex)
+            {
+                _responseDto.IsSuccess = false;
+                _responseDto.Message= ex.Message;
+            }
+
+            return _responseDto;
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public ResponseDto GetCouponById(int id)
+        {
+            try
+            {
+                Coupon coupon = _dbContext.Coupons.FirstOrDefault(x=>x.CouponId==id);
+                if (coupon == null)
+                {
+                    throw new Exception("Coupon not found");
+                }
+                CouponDto couponDto= _mapper.Map<CouponDto>(coupon);
+                _responseDto.Result= couponDto;
+                _responseDto.IsSuccess=true;
+            }
+            catch (Exception ex)
+            {
+                _responseDto.IsSuccess = false;
+                _responseDto.Message= ex.Message;
+            }
+            return _responseDto;
+        }
+
+        [HttpGet]
+        [Route("GetCouponByCode/{code}")]
+        public ResponseDto GetCouponBycode(string code)
+        {
+            try
+            {
+                Coupon coupon = _dbContext.Coupons.FirstOrDefault(x => x.CouponCode.ToLower() == code.ToLower());
+                if (coupon == null)
+                {
+                    throw new Exception("Coupon not found");
+                }
+                CouponDto couponDto = _mapper.Map<CouponDto>(coupon);
+                _responseDto.Result = couponDto;
+                _responseDto.IsSuccess = true;
+            }
+            catch (Exception ex)
+            {
+                _responseDto.IsSuccess = false;
+                _responseDto.Message = ex.Message;
+            }
+            return _responseDto;
+        }
+
+        [HttpPost]
+        public ResponseDto AddCoupon([FromBody] CouponDto couponDto)
+        {
+            try
+            {
+               Coupon coupon= _mapper.Map<Coupon>(couponDto);
+                _dbContext.Add(coupon);
+                _dbContext.SaveChanges();
+                _responseDto.Result = coupon;
+                _responseDto.IsSuccess = true;
+            }
+            catch (Exception ex)
+            {
+                _responseDto.IsSuccess = false;
+                _responseDto.Message = ex.Message;
+            }
+            return _responseDto;
+        }
+
+        [HttpPut]
+        public ResponseDto UpdateCoupon([FromBody] CouponDto couponDto)
+        {
+            try
+            {
+                Coupon coupon = _mapper.Map<Coupon>(couponDto);
+                //Coupon currentCoupon = _dbContext.Coupons.FirstOrDefault(x => x.CouponId == coupon.CouponId);
+                //if (currentCoupon == null)
+                //{
+                //    throw new Exception("Coupon not found");
+                //}
+
+                _dbContext.Update(coupon);
+                _dbContext.SaveChanges();
+                _responseDto.Result = coupon;
+                _responseDto.IsSuccess = true;
+            }
+            catch (Exception ex)
+            {
+                _responseDto.IsSuccess = false;
+                _responseDto.Message = ex.Message;
+            }
+            return _responseDto;
+        }
+
+        [HttpDelete]
+        public ResponseDto DeleteCoupon(int id)
+        {
+            try
+            {
+                Coupon coupon = _dbContext.Coupons.FirstOrDefault(x => x.CouponId == id);
+                if (coupon == null)
+                {
+                    throw new Exception("Coupon not found");
+                }
+                _dbContext.Coupons.Remove(coupon);
+                _dbContext.SaveChanges();
+                _responseDto.IsSuccess = true;
+            }
+            catch (Exception ex)
+            {
+                _responseDto.IsSuccess = false;
+                _responseDto.Message = ex.Message;
+            }
+            return _responseDto;
+        }
+
+    }
+}
---
+++ Mango.Services.CouponAPI/Data/AppDbContext.cs
@@ -0,0 +1,43 @@
+﻿using Mango.Services.CouponAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mango.Services.CouponAPI.Data
+{
+    public class AppDbContext :DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options): base(options)
+        {
+        }
+
+        public DbSet<Coupon> Coupons { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Coupon>().HasData(new Coupon()
+            {
+                CouponId = 1,
+                CouponCode = "FirstOff",
+                DiscountAmount = 10,
+                MinAmount = 500,
+            });
+
+            modelBuilder.Entity<Coupon>().HasData(new Coupon()
+            {
+                CouponId = 2,
+                CouponCode = "100Off",
+                DiscountAmount = 100,
+                MinAmount = 2000,
+            });
+
+            modelBuilder.Entity<Coupon>().HasData(new Coupon()
+            {
+                CouponId = 3,
+                CouponCode = "5PercentOff",
+                DiscountAmount = 0.05,
+                MinAmount = 1000,
+            });
+        }
+    }
+}
