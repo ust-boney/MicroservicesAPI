0949e0be0031f87379b9338024499a1db40abb61
boney84 <boney0084@gmail.com>
2024-10-31 13:57:35 +0530

    Added Mango.Web Client project and setup the service client

---
+++ Mango.Web/Services/BaseService.cs
@@ -0,0 +1,74 @@
+﻿using Mango.Web.Models;
+using Mango.Web.Services.IService;
+using Mango.Web.Utility;
+using Newtonsoft.Json;
+using System.Net;
+using System.Text;
+
+namespace Mango.Web.Services
+{
+    public class BaseService : IBaseService
+    {
+        private readonly IHttpClientFactory _httpClientFactory; 
+        public BaseService(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<ResponseDto?> SendAsync(RequestDto requestDto)
+        {
+            try
+            {
+                HttpClient httpClient = _httpClientFactory.CreateClient("Mango");
+                HttpRequestMessage message = new();
+                message.Headers.Add("Content-Type", "application/json");
+                message.RequestUri = new Uri(requestDto.Url);
+                if (requestDto.Data != null)
+                {
+                    message.Content = new StringContent(JsonConvert.SerializeObject(requestDto.Data), Encoding.UTF8, "application/json");
+                }
+
+                HttpResponseMessage apiResponse = null;
+                switch (requestDto.ApiType)
+                {
+                    case ApiType.POST:
+                        message.Method = HttpMethod.Post;
+                        break;
+                    case ApiType.PUT:
+                        message.Method = HttpMethod.Put;
+                        break;
+                    case ApiType.DELETE:
+                        message.Method = HttpMethod.Delete;
+                        break;
+                    default:
+                        message.Method = HttpMethod.Get;
+                        break;
+                }
+
+                apiResponse = await httpClient.SendAsync(message);
+                switch (apiResponse.StatusCode)
+                {
+                    case HttpStatusCode.NotFound:
+                        return new ResponseDto() { IsSuccess = false, Message = "Not Found" };
+                    case HttpStatusCode.Unauthorized:
+                        return new ResponseDto() { IsSuccess = false, Message = "User not authorized" };
+                    case HttpStatusCode.Forbidden:
+                        return new ResponseDto() { IsSuccess = false, Message = "Access denied" };
+                    case HttpStatusCode.InternalServerError:
+                        return new ResponseDto() { IsSuccess = false, Message = "Internal Server Error" };
+
+                    default:
+                        var apiContent = await apiResponse.Content.ReadAsStringAsync();
+                        var content = JsonConvert.DeserializeObject<ResponseDto>(apiContent);
+
+                        return content;
+                }
+            }
+            catch (Exception ex)
+            {
+                return new ResponseDto() { IsSuccess = false,Message=ex.Message };
+            }
+ 
+            
+        }
+    }
+}
---
+++ Mango.Web/Utility/StandardEnum.cs
@@ -0,0 +1,11 @@
+﻿namespace Mango.Web.Utility
+{
+    public enum ApiType
+    {
+        GET,
+        POST,
+        PUT,
+        DELETE,
+        PATCH
+    }
+}
